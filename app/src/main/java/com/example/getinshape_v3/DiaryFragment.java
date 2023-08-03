344f3f93391016054e09da0d884e146533a35612
Rafael Bastos <rafabastos@gmail.com>
2023-08-03 16:26:36 -0700

    RecyclerAdapter.java deleted, Using FoodAdapter

--- app/src/main/java/com/example/getinshape_v3/DiaryFragment.java
+++ app/src/main/java/com/example/getinshape_v3/DiaryFragment.java
@@ -1,7 +1,6 @@
 package com.example.getinshape_v3;
 
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,18 +14,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import com.example.getinshape_v3.Adapter.FoodAdapter;
-import com.example.getinshape_v3.Adapter.RecyclerAdapter;
 import com.example.getinshape_v3.FoodModel.FoodModel;
 import com.example.getinshape_v3.Utils.DataBaseFoodHelper;
 import com.example.getinshape_v3.Utils.DataBaseUserHelper;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class DiaryFragment extends Fragment implements onDialogCloseListener {
 
@@ -34,19 +29,6 @@ public class DiaryFragment extends Fragment implements onDialogCloseListener {
     DataBaseFoodHelper dataBaseFoodHelper;
     DataBaseUserHelper dataBaseUserHelper;
 
-    Button delete_button;
-
-    ArrayList<String> food_name, serving_size, calories;
-    ArrayList<Long> timestamp;
-
-    RecyclerView.LayoutManager layoutManager;
-
-//    RecyclerView.Adapter adapter;
-
-
-    // TEST
-    MainActivity activity;
-//    RecyclerAdapter adapter;
     FoodAdapter foodAdapter;
     ArrayList<FoodModel> mList;
 
@@ -70,6 +52,7 @@ public class DiaryFragment extends Fragment implements onDialogCloseListener {
         // THIS CHUNK OF CODE BELOW IS WORKING
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
         dataBaseFoodHelper = new DataBaseFoodHelper(getActivity());
+        dataBaseUserHelper = new DataBaseUserHelper(getActivity());
         mList = new ArrayList<>();
         foodAdapter = new FoodAdapter(dataBaseFoodHelper,  getActivity());
 
@@ -82,78 +65,23 @@ public class DiaryFragment extends Fragment implements onDialogCloseListener {
         // THIS CHUNK OF CODE ABOVE IS WORKING
 
 
+        calories_eaten_todayTV = (TextView) getView().findViewById(R.id.calories_eaten_todayTV);
+        calorie_targetTV = (TextView) getView().findViewById(R.id.calorie_targetTV);
+        calories_remainingTV = (TextView) getView().findViewById(R.id.calories_remainingTV);
+
+        try {
 
 
+            loadCalorieIntake();
 
+            loadRecommendedIntake();
 
-
-
-//        Intent intent = getActivity().getIntent();
-//        Bundle b = intent.getExtras();
-//        currentUserEmail = b.getString("currentUserEmail");
-//
-//        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-
-        // TODO: THIS CAN BE GIVING ERROR - CLASSWORK 15, 27 MIN
-//        dataBaseFoodHelper = new DataBaseFoodHelper(getActivity().getApplicationContext());
-//        dataBaseUserHelper = new DataBaseUserHelper(getActivity().getApplicationContext());
-//        dataBaseFoodHelper = new DataBaseFoodHelper(getActivity());
-//        dataBaseUserHelper = new DataBaseUserHelper(getActivity());
-
-//        food_name = new ArrayList<>();
-//        serving_size = new ArrayList<>();
-//        calories = new ArrayList<>();
-//        timestamp = new ArrayList<>();
-
-
-//        final LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-//        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-//        mRecyclerView.setLayoutManager(layoutManager);
-//
-//        data = dataBaseFoodHelper.getAllFood();
-//
-//        foodAdapter = new FoodAdapter(data);
-//        mRecyclerView.setAdapter(foodAdapter);
-
-
-
-//        adapter = new RecyclerAdapter(getActivity().getApplicationContext(), food_name, serving_size, calories, timestamp);
-////        adapter = new FoodAdapter(dataBaseFoodHelper, getActivity());
-//        mRecyclerView.setAdapter(adapter);
-//        layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
-//        mRecyclerView.setLayoutManager(layoutManager);
-
-//        calories_eaten_todayTV = (TextView) getView().findViewById(R.id.calories_eaten_todayTV);
-//        calorie_targetTV = (TextView) getView().findViewById(R.id.calorie_targetTV);
-//        calories_remainingTV = (TextView) getView().findViewById(R.id.calories_remainingTV);
-//
-//        try {
-//            loadFoodData();
-//
-//            loadCalorieIntake();
-//
-//            loadRecommendedIntake();
-//
-//            double calories_remaining = Double.parseDouble(calorie_target_str) - Double.parseDouble(calories_eaten_str);
-//            String calories_remaining_str = String.format("%.2f", calories_remaining);
-//            calories_remainingTV.setText(calories_remaining_str);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-////            Toast.makeText(getActivity().getApplicationContext(), "Oops, the diary is empty", Toast.LENGTH_LONG).show();
-//        }
-
-    }
-
-    public void loadFoodData() {
-
-        //Retrieve the data from the database
-        Cursor result = dataBaseFoodHelper.getUserFoodData();
-
-        while (result.moveToNext()) {
-            timestamp.add(result.getLong(0));
-            food_name.add(result.getString(2));
-            serving_size.add(result.getString(3));
-            calories.add(result.getString(4));
+            double calories_remaining = Double.parseDouble(calorie_target_str) - Double.parseDouble(calories_eaten_str);
+            String calories_remaining_str = String.format("%.2f", calories_remaining);
+            calories_remainingTV.setText(calories_remaining_str);
+        } catch (Exception e) {
+            e.printStackTrace();
+//            Toast.makeText(getActivity().getApplicationContext(), "Oops, the diary is empty", Toast.LENGTH_LONG).show();
         }
 
     }
